1169368e79173b2a3e492a217a6e6a5cb09d7565
Nika <example@gmail.com>
2020-12-13 21:12:26 +0200

    Object destruction

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 "use strict";
 
-function first(){
-    //
-    setTimeout(function(){
-        console.log(1);
-    },500);
+const options={
+    name:'test',
+    width:1024,
+    height:1024,
+    colors:{
+        border:'black',
+        bg:'red'
+    },
+    makeTest:function(){
+        console.log("Test!");
+    }
+};
+options.makeTest();
+
+const {border,bg}=options.colors;
+console.log(border);
+
+//console.log(Object.keys(options).length);
+
+/*
+console.log(options.name);
+
+delete options.name;
+console.log(options);
+*/
+let counter=0;
+for (let key in options){
+    if (typeof(options[key])=='object'){
+        console.log(`свойство ${key} имеет значение Object:`);
+        for (let i in options[key]){
+            console.log(`свойство ${i} имеет значение ${options[key][i]}`); 
+            counter++;
+        }
+    } else {
+    console.log(`свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+    }
 }
-
-function second(){
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang,callback){
-    console.log(`I learn: ${lang}`);
-    callback();
-}
-
-function myCallback(){
-    console.log('lesson is done'); 
-}
-
-learnJS('JavaScript', myCallback);
+console.log(counter);
