f551ba4cd1690cf98a42d5447c089be2f50a266c
Nika <example@gmail.com>
2020-12-14 21:07:39 +0200

    Object Array clone copy end ...

--- js/script.js
+++ js/script.js
@@ -1,39 +1,90 @@
 "use strict";
 
-const arr=[10,2,3,6,8];
+let a=5, b=a;
 
-arr.sort(compareNum);
-console.log(arr);
 
-function compareNum(a,b){
-    return a-b;
-}
-/*
-arr[99]=0;
-console.log(arr.length);
-console.log(arr);
-*/
-arr.forEach(function(item,i,arr){
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+b=b+5;
+console.log(b);
+console.log(a);
 
-/*arr.pop();
+const obj={
+    a:5,
+    b:1
+};
 
-console.log(arr);
+const copy=obj;
 
-arr.push(10);
+copy.a=10;
 
-console.log(arr);*/
-/*
-for (let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-}
-*/
-for(let value of arr){
-    console.log(value);
+console.log(copy);
+console.log(obj);
+
+function copyObj(mainObj){
+    let objCopy={};
+
+    let key;
+    for (key in mainObj){
+        objCopy[key]=mainObj[key];
+    }
+
+    return objCopy;
 }
 
-const str=prompt("","");
-const products=str.split(",");
-products.sort();
-console.log(products.join('; '));
+const numbers = {
+    a:2,
+    b:5,
+    c:{
+        x:7,
+        y:4
+    }
+};
+
+const newNumbers=copyObj(numbers);
+newNumbers.c.x=10;
+
+console.log(newNumbers);
+
+const add={
+    d:17,
+    e:20
+};
+
+const clone=Object.assign({},add);
+clone.d=20;
+console.log(add);
+console.log(clone);
+
+const oldArr=['a','b','c'];
+const newArr=oldArr.slice();
+newArr[1]='uyuyuyuyuy';
+console.log(newArr);
+
+//оператор разворота, т.е. разбор по частям:
+const video=['youtube','vimeo','rutube'],
+    blogs=['wordpress','livejournal','blogger'],
+    internet=[...video, ...blogs, 'vk','facebook'];
+
+console.log(internet);
+
+function log(a,b,c){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num=[2,5,7];
+
+log(...num);
+
+const array=["a","b"];
+
+const newArray=[...array];
+
+const q={
+    one:1,
+    two:2
+};
+
+const newObj={...q};
+newObj.two=5;
+console.log(newArray,newObj,q);
